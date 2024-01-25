2aacf3ce27c19f40d2bdbef3a42a8b42c863dda1
William Zheng <williamzheng2001@gmail.com>
2024-01-25 13:02:49 -0500

    Remove express project

--- express-app/src/app.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-export default app;
--- express-app/src/index.ts
+++
@@ -1,6 +0,0 @@
-import "dotenv/config";
-import app from "./app";
-
-const port = process.env.PORT || 8000;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
