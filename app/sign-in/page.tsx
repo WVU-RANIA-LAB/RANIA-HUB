d3ff06ba0026c80b52f4927a5fd22caf1d03c0cf
William Zheng <williamzheng2001@gmail.com>
2024-02-19 21:21:12 -0500

    Make input text darker

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -35,7 +35,7 @@ export default function Page() {
             <input
               type="text"
               placeholder="name@gmail.com"
-              className="bg-white placeholder:text-gray-500"
+              className="bg-white text-gray-800 placeholder:text-gray-500"
             />
           </label>
           <button className="btn rounded-md border-none bg-wvu-blue text-lg font-semibold text-white hover:bg-wvu-primary-blue">
