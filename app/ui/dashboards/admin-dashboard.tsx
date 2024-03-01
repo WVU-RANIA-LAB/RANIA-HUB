62c47ef95ae8facf6089eccd49c5e44cf0a45d75
Sarth Patel <sarthbpatel@gmail.com>
2024-03-01 17:01:14 -0500

    Updated admin dashboard page

--- app/ui/dashboards/admin-dashboard.tsx
+++ app/ui/dashboards/admin-dashboard.tsx
@@ -1,5 +1,16 @@
 import NavBar from '@/app/components/admin-navbar';
 
+NavBar;
 export default function AdminDashboard() {
-  return <NavBar />;
+  return (
+    <NavBar
+      links={[
+        // Add more links as needed. The example is given below
+        // { href: '/dashboard/home', label: 'Home' },
+        { href: '#', label: 'Residents' },
+        { href: '#', label: 'Doctors' },
+        { href: '#', label: 'Admins' },
+      ]}
+    />
+  );
 }
