43cb7ec1df2d7610dd00950c0a7803f195cbf457
William Zheng <williamzheng2001@gmail.com>
2024-03-10 17:06:14 -0400

    Extends authjs interfaces

--- auth.ts
+++ auth.ts
@@ -1,8 +1,22 @@
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import {} from 'next-auth/jwt';
 
 import authConfig from './auth.config';
 import prisma from '@/app/lib/prisma';
+import { Role } from '@prisma/client';
+
+declare module 'next-auth' {
+  interface User {
+    role: Role;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    role?: Role;
+  }
+}
 
 export const {
   handlers: { GET, POST },
