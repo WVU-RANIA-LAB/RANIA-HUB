a8e915023b8f79e09156fe60fdf1fd275e61b67a
William Zheng <williamzheng2001@gmail.com>
2024-03-11 19:29:18 -0400

    Update admin profile route

--- app/(dashboards)/admin-dashboard/profile/page.tsx
+++ app/(dashboards)/admin-dashboard/profile/page.tsx
@@ -1,9 +1,21 @@
-import Profile from '@/app/ui/admin-profile';
+import { redirect } from 'next/navigation';
 
-export default function Page() {
+import { auth } from '@/auth';
+import { fetchUserByEmail } from '@/app/lib/data';
+import ProfileForm from '@/app/ui/profile-form';
+
+export default async function Page() {
+  const session = await auth();
+
+  if (!session?.user?.email) {
+    redirect('/navigation');
+  }
+
+  const user = await fetchUserByEmail(session.user.email);
+
   return (
-    <div>
-      <Profile />
-    </div>
+    <main className="grow bg-white pt-16">
+      <ProfileForm user={user} />
+    </main>
   );
 }
