c0b6aa83e3cc93d2a4a1fb077824d0e88fc5c118
William Zheng <williamzheng2001@gmail.com>
2024-03-11 10:39:15 -0400

    Remove unnecessary components

--- app/ui/dashboards/admin-dashboard.tsx
+++
@@ -1,5 +0,0 @@
-import Navbar from '@/app/ui/navbar';
-
-export default function AdminDashboard() {
-  return <Navbar role="ADMIN" />;
-}
--- app/ui/dashboards/resident-dashboard.tsx
+++
@@ -1,3 +0,0 @@
-export default function ResidentDashboard() {
-  return <div>Resident dashboard</div>;
-}
