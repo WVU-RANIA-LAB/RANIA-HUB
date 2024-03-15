3778dc3c21be501ef07e4b99de0b490377fd167f
William Zheng <williamzheng2001@gmail.com>
2024-03-15 10:49:18 -0400

    Implement doctor profile page

---
+++ app/(dashboards)/doctor-dashboard/profile/page.tsx
@@ -0,0 +1,21 @@
+import { Metadata } from 'next';
+
+import { auth } from '@/auth';
+import { fetchUserByEmail } from '@/app/lib/data';
+import ProfileForm from '@/app/ui/profile-form';
+
+export const metadata: Metadata = {
+  title: 'Profile',
+};
+
+export default async function Page() {
+  const session = await auth();
+
+  const user = await fetchUserByEmail(session!.user!.email!);
+
+  return (
+    <main className="grow bg-white py-16">
+      <ProfileForm user={user} />
+    </main>
+  );
+}
