68ad0fea88fb29427cd936c09ed260fd05457026
Mbehanna2019 <mlb0094@mix.wvu.edu>
2024-03-18 18:15:38 -0400

    Continuing Progress on Contacts Page
    Quick update push

--- app/(dashboards)/resident-dashboard/contacts/page.tsx
+++ app/(dashboards)/resident-dashboard/contacts/page.tsx
@@ -1,11 +1,19 @@
 export default function Page() {
     return <main>
 
-    <div style={{ position: 'absolute', left: '25px', top: '135px', width: '1300px', height: '450px', border: '1px solid black', backgroundColor: '#F6EED8'  }}>
-        <div style={{ width: '100%', height: '50px', backgroundColor: '#002855' }}>
-            <h3 style={{ margin: 1 }}>Contacts</h3>
+    <div style={{ position: 'absolute', left: '30px', top: '100px', width: '95%', height: '80%', border: '1px solid black', backgroundColor: '#F6EED8'  }}>
+        
+        
+        <div style={{ display: 'flex', width: '100%', height: '10%', backgroundColor: '#002855' }}>
+            <h6 style={{ margin: 1, fontSize: '24px', fontWeight: 'bold' }}>Contacts</h6>
         </div>
-        <h1>Contact 1</h1>
+
+        <h1 style={{ margin: 1}}>
+            *Contact Fields*
+        </h1>
+
+        *Line Placeholder*
+
     </div>
 
 
