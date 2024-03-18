2429537a62a30852b32cd9a3e800c256dfbdca25
Mbehanna2019 <mlb0094@mix.wvu.edu>
2024-03-18 15:36:55 -0400

    Started the Resident Contacts Page
    Created the container relevant to the Figma for the Contacts page for Residents

---
+++ app/(dashboards)/resident-dashboard/contacts/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+    return <main>
+
+    <div style={{ position: 'absolute', left: '25px', top: '135px', width: '1300px', height: '450px', border: '1px solid black', backgroundColor: '#F6EED8'  }}>
+        <div style={{ width: '100%', height: '50px', backgroundColor: '#002855' }}>
+            <h3 style={{ margin: 1 }}>Contacts</h3>
+        </div>
+        <h1>Contact 1</h1>
+    </div>
+
+
+    </main>;
+  }
