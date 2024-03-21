2fc1f10b901726da212eef25c3dc5d22a9724e14
William Zheng <williamzheng2001@gmail.com>
2024-03-21 10:37:15 -0400

    Render search and table

--- app/(dashboards)/doctor-dashboard/residents/page.tsx
+++ app/(dashboards)/doctor-dashboard/residents/page.tsx
@@ -1,3 +1,36 @@
-export default function Page() {
-  return <main>Doctor Dashboard Placeholder</main>;
+import { Metadata } from 'next';
+
+import { auth } from '@/auth';
+import { fetchUserByEmail } from '@/app/lib/data/data';
+import Search from '@/app/ui/search';
+import ResidentsTable from '@/app/ui/doctor-dashboard/residents-table';
+
+export const metadata: Metadata = {
+  title: 'Residents',
+};
+
+type PageProps = {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+};
+
+export default async function Page({ searchParams }: PageProps) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const session = await auth();
+  const doctor = await fetchUserByEmail(session!.user!.email!);
+
+  return (
+    <main className="grow bg-white">
+      <Search placeholder="Search residents..." />
+      <ResidentsTable
+        residentIds={doctor.residentIds}
+        query={query}
+        currentPage={currentPage}
+      />
+    </main>
+  );
 }
