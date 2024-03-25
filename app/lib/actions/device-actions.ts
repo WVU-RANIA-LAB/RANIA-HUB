b79cc346440898a645cd037aee7b0ee646c11edf
Kerrigan <kmr00004@mix.wvu.edu>
2024-03-24 21:38:55 -0400

    added device actions

--- app/lib/actions/device-actions.ts
+++ app/lib/actions/device-actions.ts
@@ -1,7 +1,105 @@
 'use server';
 
-export async function addDevice() {}
+import { revalidatePath } from 'next/cache';
+import { z } from 'zod';
 
-export async function editDevice() {}
+import prisma from '@/app/lib/prisma';
+import { User } from '@prisma/client';
 
-export async function deleteDevice() {}
+const DeviceFormSchema = z.object({
+  name: z.string().trim().min(1, { message: 'Name required' }),
+  description: z.string().trim().min(1, { message: 'Description required' }),
+});
+
+export type DeviceFormState = {
+  message: string | null;
+  errors: {
+    name?: string[];
+    description?: string[];
+  };
+};
+
+export async function addDevice(
+  user: User,
+  _prevState: DeviceFormState,
+  formData: FormData,
+): Promise<DeviceFormState> {
+  const validatedFields = DeviceFormSchema.safeParse(
+    Object.fromEntries(formData.entries()),
+  );
+
+  if (!validatedFields.success) {
+    return {
+      message: 'Invalid/Missing fields. Failed to create device.',
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+
+  const { data } = validatedFields;
+
+  try {
+    await prisma.device.create({
+      data: {
+        name: data.name,
+        status: 'off',
+        description: data.description,
+        belongsToId: user.id,
+      },
+    });
+  } catch (e) {
+    return { message: 'Database Error: Failed to add device.', errors: {} };
+  }
+
+  revalidatePath(`/resident-dashboard/devices`);
+  return { message: 'Successfully added device.', errors: {} };
+}
+
+export async function editDevice(
+  id: string,
+  _prevState: DeviceFormState,
+  formData: FormData,
+): Promise<DeviceFormState> {
+  const validatedFields = DeviceFormSchema.safeParse(
+    Object.fromEntries(formData.entries()),
+  );
+
+  if (!validatedFields.success) {
+    return {
+      message: 'Invalid/Missing fields. Failed to update device.',
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+
+  const { data } = validatedFields;
+
+  try {
+    await prisma.device.update({
+      where: { id },
+      data: {
+        name: data.name,
+        description: data.description,
+      },
+    });
+  } catch (e) {
+    return { message: 'Database Error: Failed to update device.', errors: {} };
+  }
+
+  revalidatePath(`/resident-dashboard/devices`);
+  return { message: 'Successfully updated device.', errors: {} };
+}
+
+export async function deleteDevice(
+  id: string,
+  _prevState: DeviceFormState,
+): Promise<DeviceFormState> {
+  try {
+    await prisma.device.delete({
+      where: { id },
+    });
+  } catch (e) {
+    return { message: 'Database Error: Failed to delete device.', errors: {} };
+  }
+
+  revalidatePath(`/resident-dashboard/devices`);
+  return { message: 'Successfully deleted device.', errors: {} };
+}
