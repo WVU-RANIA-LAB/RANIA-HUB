cf6f8fdf1fc7c69f946d562a46633932348ee591
Mbehanna2019 <mlb0094@mix.wvu.edu>
2024-03-25 16:47:26 -0400

    Changed the styling to Kerrigans recommendation
    New styling done for contacts and medications

--- app/ui/resident-ui/contacts.tsx
+++ app/ui/resident-ui/contacts.tsx
@@ -1,32 +1,41 @@
 export default function Contacts() {
     return (
-        <main>
-            <div className="mt-4 mb-4 mr-4 ml-4 min-h-screen flex flex-col">
-                
-                <div className="flex-grow mb-24 bg-white">
-                    
-                    <div className="flex items-center justify-between w-full h-12 bg-blue-900 pl-5 pr-5">
-                        <h6 className=" text-2xl font-bold text-white">Contacts</h6>
-                            <a href="#" className="text-blue-900 no-underline">
-                                <button className="w-24 h-8 text-sm rounded-full bg-white flex justify-center items-center border-2 border-black font-bold">
-                                    Add Contact
-                                </button>
-                            </a>
-                    </div>
-
-                <div className="pt-5 pl-5">
-                    <span className="text-blue-900 text-base mr-32 font-bold">Name:</span>
-                    <span className="text-blue-900 text-base mr-56 font-bold">Address:</span>
-                    <span className="text-blue-900 text-base mr-60 font-bold">Phone Number:</span>
-                    <span className="text-blue-900 text-base mr-80 font-bold">Email:</span>
-                    <span className="text-blue-900 text-base font-bold">Actions:</span>
-                </div>
-
-                <div className="border border-black mx-4"></div>
-
-                </div>
-            </div>
-
-        </main>
+      <main className="grow bg-white py-16">
+        <div className="mx-auto max-w-7xl">
+          
+          <div className={`rounded border-8 border-wvu-primary-blue bg-wvu-primary-blue text-2xl font-bold text-white antialiased flex items-center justify-between`}>
+            <span>Manage Contacts</span>
+            <button className="w-32 h-8 text-sm rounded-full bg-white flex justify-center items-center border-2 border-black font-bold hover:bg-gold-500 hover:text-white">
+              <tr className={'text-lg font-bold text-wvu-primary-blue antialiased'}>
+                Add Contact
+              </tr>
+            </button>
+          </div>
+  
+          <br />
+  
+          <table className="table table-auto">
+            <thead>
+              <tr className={`text-2xl font-bold text-wvu-primary-blue antialiased`}>
+                <th>Name</th>
+                <th>Address</th>
+                <th>Phone Number</th>
+                <th>Email</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="text-lg text-black">
+                <td>Filler</td>
+                <td>Filler</td>
+                <td>Filler</td>
+                <td>Filler</td>
+                <td>Filler</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </main>
     );
-}
+  }
+  
