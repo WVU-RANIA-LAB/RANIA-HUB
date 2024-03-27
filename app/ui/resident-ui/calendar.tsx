f51aae11eec91070cff8d96d51bfb988095f421a
Mbehanna2019 <mlb0094@mix.wvu.edu>
2024-03-27 14:48:10 -0400

    Fixed Issues with Calendar Page
    Navbar now shows up when navigated to the Calendar page. Now I need to implement a pop up button to add/delete events

--- app/ui/resident-ui/calendar.tsx
+++ app/ui/resident-ui/calendar.tsx
@@ -1,11 +1,13 @@
+import React from 'react';
+
 export default function Calendar() {
     return (
-
-        <body className="bg-gray-100">
-            <div className="container mx-auto">
-                <iframe src="https://calendar.google.com/calendar/embed?src=mlb0094@mix.wvu.edu" className="w-full h-screen"></iframe>
+        <div>
+            <div className="grow py-16 bg-wvu-primary-blue">
+                <div className="container mx-auto">
+                    <iframe src="https://calendar.google.com/calendar/embed?src=mlb0094@mix.wvu.edu" className="w-full h-screen"></iframe>
+                </div>
             </div>
-        </body>
-
+        </div>
     );
 }
