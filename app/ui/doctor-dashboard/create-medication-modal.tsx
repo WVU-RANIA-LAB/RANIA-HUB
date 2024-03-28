ce975e4975778f876d87ef5b45a086d6e4f37f2f
William Zheng <williamzheng2001@gmail.com>
2024-03-28 18:41:34 -0400

    Style form

--- app/ui/doctor-dashboard/create-medication-modal.tsx
+++ app/ui/doctor-dashboard/create-medication-modal.tsx
@@ -2,6 +2,8 @@
 
 import { useRef } from 'react';
 import { useFormState, useFormStatus } from 'react-dom';
+import { XMarkIcon } from '@heroicons/react/24/outline';
+
 import { createMedication } from '@/app/lib/actions/medication-actions';
 
 type CreateMedicationModalProps = { doctorId: string; residentId: string };
@@ -27,15 +29,53 @@ export default function CreateMedicationModal({
       <button className="btn" onClick={() => dialogRef.current?.showModal()}>
         Create
       </button>
+
       <dialog ref={dialogRef} className="modal">
         <div className="modal-box">
-          <form action={dispatch}>
-            <input type="text" name="name" placeholder="Medication name" />
-            <textarea name="instructions" />
-            <input type="number" name="refills" placeholder="Refills" />
+          <form method="dialog">
+            <button className="btn btn-circle btn-ghost btn-sm absolute right-6 top-6">
+              <XMarkIcon className="h-6" />
+            </button>
+          </form>
+          <h2 className="mb-4 text-lg font-bold">Create Medication</h2>
+
+          <form action={dispatch} className="flex flex-col gap-y-4">
+            <label className="form-control">
+              <div className="label">
+                <span className="label-text">Medication Name:</span>
+              </div>
+              <input
+                type="text"
+                name="name"
+                placeholder="Medication name"
+                className="input input-bordered"
+              />
+            </label>
+            <label className="form-control">
+              <div className="label">
+                <span className="label-text">Instructions:</span>
+              </div>
+              <textarea
+                name="instructions"
+                placeholder="Instructions"
+                className="textarea textarea-bordered h-32"
+              />
+            </label>
+            <label className="form-control">
+              <div className="label">
+                <span className="label-text">Refills</span>
+              </div>
+              <input
+                type="number"
+                name="refills"
+                placeholder="Refills"
+                className="input input-bordered"
+              />
+            </label>
             <Submit />
           </form>
         </div>
+
         <form method="dialog" className="modal-backdrop">
           <button>close</button>
         </form>
@@ -47,5 +87,10 @@ export default function CreateMedicationModal({
 function Submit() {
   const { pending } = useFormStatus();
 
-  return <button disabled={pending}>Create</button>;
+  return (
+    <button disabled={pending} className="btn mt-4">
+      {pending && <span className="loading loading-spinner" />}
+      Create
+    </button>
+  );
 }
