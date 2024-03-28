bdbd68b7a7961af11a0e85f37c548e7fd1314b27
William Zheng <williamzheng2001@gmail.com>
2024-03-28 15:41:02 -0400

    Write function to fetch user by id

--- app/lib/data/data.ts
+++ app/lib/data/data.ts
@@ -2,6 +2,18 @@ import { unstable_noStore as noStore } from 'next/cache';
 
 import prisma from '@/app/lib/prisma';
 
+export async function fetchUserById(id: string) {
+  noStore();
+
+  try {
+    return prisma.user.findUniqueOrThrow({
+      where: { id },
+    });
+  } catch (e) {
+    throw new Error('Failed to fetch user');
+  }
+}
+
 export async function fetchUserByEmail(email: string) {
   noStore();
 
