b4bc0b8f2a59fe8c93e52cd20029f5eac66e045e
William Zheng <williamzheng2001@gmail.com>
2024-03-29 16:03:26 -0400

    Add function to fetch medical history

--- app/lib/data/doctor-data.ts
+++ app/lib/data/doctor-data.ts
@@ -133,3 +133,35 @@ export async function fetchMedicationsPages(residentId: string, query: string) {
     throw new Error('Failed to fetch total number of medications');
   }
 }
+
+export async function fetchFilteredMedicalHistoryEntries(
+  residentId: string,
+  query: string,
+  currentPage: number,
+) {
+  noStore();
+
+  try {
+    const medicalHistoryEntries = await prisma.medicalHistoryEntry.findMany({
+      skip: (currentPage - 1) * ITEMS_PER_PAGE,
+      take: ITEMS_PER_PAGE,
+      where: {
+        AND: [
+          { residentId },
+          { description: { contains: query, mode: 'insensitive' } },
+        ],
+      },
+      select: {
+        id: true,
+        date: true,
+        description: true,
+        editor: true,
+      },
+      orderBy: { date: 'asc' },
+    });
+    return medicalHistoryEntries;
+  } catch (e) {
+    console.error('Database Error:', e);
+    throw new Error('Failed to fetch medical history entries.');
+  }
+}
