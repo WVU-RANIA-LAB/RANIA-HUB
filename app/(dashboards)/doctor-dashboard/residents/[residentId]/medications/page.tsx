548ada6824ec54d8c814a93038330f7f3be683ae
William Zheng <williamzheng2001@gmail.com>
2024-03-29 12:07:18 -0400

    Update import

--- app/(dashboards)/doctor-dashboard/residents/[residentId]/medications/page.tsx
+++ app/(dashboards)/doctor-dashboard/residents/[residentId]/medications/page.tsx
@@ -5,7 +5,7 @@ import { fetchMedicationsPages } from '@/app/lib/data/doctor-data';
 import Search from '@/app/ui/search';
 import MedicationsTable from '@/app/ui/doctor-dashboard/medications-table';
 import Pagination from '@/app/ui/pagination';
-import CreateMedicationModal from '@/app/ui/doctor-dashboard/create-medication-modal';
+import CreateMedicationButton from '@/app/ui/doctor-dashboard/create-medication-btn';
 
 type PageProps = {
   params: { residentId: string };
@@ -30,7 +30,7 @@ export default async function Page({ params, searchParams }: PageProps) {
         <h1 className={`${lusitana.className} text-3xl antialiased`}>
           {resident.name}&apos;s Medications
         </h1>
-        <CreateMedicationModal doctorId={doctor.id} residentId={residentId} />
+        <CreateMedicationButton doctorId={doctor.id} residentId={residentId} />
       </div>
       <Search placeholder="Search medications..." />
       <MedicationsTable
