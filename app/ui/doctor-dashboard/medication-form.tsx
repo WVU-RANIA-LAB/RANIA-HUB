e6fd8f758867e9b5dacd0feb4c76817e6f53edc7
William Zheng <williamzheng2001@gmail.com>
2024-03-29 15:21:27 -0400

    Show form errors

--- app/ui/doctor-dashboard/medication-form.tsx
+++ app/ui/doctor-dashboard/medication-form.tsx
@@ -8,6 +8,7 @@ import {
   updateMedication,
 } from '@/app/lib/actions/medication-actions';
 import { fetchFilteredMedications } from '@/app/lib/data/doctor-data';
+import FieldErrors from '@/app/ui/field-errors';
 
 type MedicationFormProps = {
   doctorId: string;
@@ -24,42 +25,52 @@ const MedicationForm = forwardRef<HTMLFormElement, MedicationFormProps>(
 
     return (
       <form ref={ref} action={dispatch} className="flex flex-col gap-y-4">
-        <label className="form-control">
-          <div className="label">
-            <span className="label-text">Medication Name:</span>
-          </div>
-          <input
-            type="text"
-            name="name"
-            placeholder="Medication name"
-            defaultValue={medication?.name ?? ''}
-            className="input input-bordered"
-          />
-        </label>
-        <label className="form-control">
-          <div className="label">
-            <span className="label-text">Instructions:</span>
-          </div>
-          <textarea
-            name="instructions"
-            placeholder="Instructions"
-            defaultValue={medication?.instructions ?? ''}
-            className="textarea textarea-bordered h-32"
-          />
-        </label>
-        <label className="form-control">
-          <div className="label">
-            <span className="label-text">Refills</span>
-          </div>
-          <input
-            type="number"
-            name="refills"
-            placeholder="Refills"
-            min={0}
-            defaultValue={medication?.refills ?? ''}
-            className="input input-bordered"
-          />
-        </label>
+        <div>
+          <label className="form-control">
+            <div className="label">
+              <span className="label-text">Medication Name:</span>
+            </div>
+            <input
+              type="text"
+              name="name"
+              placeholder="Medication name"
+              defaultValue={medication?.name ?? ''}
+              className="input input-bordered"
+              required
+            />
+          </label>
+          <FieldErrors errors={state.errors?.name} />
+        </div>
+        <div>
+          <label className="form-control">
+            <div className="label">
+              <span className="label-text">Instructions:</span>
+            </div>
+            <textarea
+              name="instructions"
+              placeholder="Instructions"
+              defaultValue={medication?.instructions ?? ''}
+              className="textarea textarea-bordered h-32"
+            />
+          </label>
+          <FieldErrors errors={state.errors?.instructions} />
+        </div>
+        <div>
+          <label className="form-control">
+            <div className="label">
+              <span className="label-text">Refills</span>
+            </div>
+            <input
+              type="number"
+              name="refills"
+              placeholder="Refills"
+              min={0}
+              defaultValue={medication?.refills ?? ''}
+              className="input input-bordered"
+            />
+          </label>
+          <FieldErrors errors={state.errors?.refills} />
+        </div>
         <Submit mode={medication ? 'Update' : 'Create'} />
       </form>
     );
