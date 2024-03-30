22038a5f642f92f5240d910d3a74b55adc9b11ad
William Zheng <williamzheng2001@gmail.com>
2024-03-30 08:42:06 -0400

    Create medical history table

---
+++ app/ui/doctor-dashboard/medical-history-table.tsx
@@ -0,0 +1,60 @@
+import { fetchFilteredMedicalHistoryEntries } from '@/app/lib/data/doctor-data';
+
+type MedicalHistoryTableProps = {
+  doctorId: string;
+  residentId: string;
+  query: string;
+  currentPage: number;
+};
+
+export default async function MedicalHistoryTable({
+  doctorId,
+  residentId,
+  query,
+  currentPage,
+}: MedicalHistoryTableProps) {
+  const medicalHistoryEntries = await fetchFilteredMedicalHistoryEntries(
+    residentId,
+    query,
+    currentPage,
+  );
+
+  const formatter = new Intl.DateTimeFormat('en-US');
+
+  return (
+    <div className="overflow-x-auto">
+      <table className="table hidden md:table">
+        <thead>
+          <tr>
+            <th scope="col">Date</th>
+            <th scope="col">Description</th>
+            <th scope="col">Added By</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {medicalHistoryEntries.map((entry) => (
+            <tr key={entry.id}>
+              <td>{formatter.format(entry.date)}</td>
+              <td>{entry.description}</td>
+              <td>{entry.editor.name}</td>
+              <td className="flex gap-2">
+                {/*
+                <EditMedicationButton
+                  doctorId={doctorId}
+                  residentId={residentId}
+                  medication={entry}
+                />
+                <DeleteMedicationButton
+                  medicationId={entry.id}
+                  residentId={residentId}
+                />
+                */}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
