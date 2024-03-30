4cf77d50ee08c7c08477e8bf5af7d0f1040542e4
William Zheng <williamzheng2001@gmail.com>
2024-03-30 08:52:52 -0400

    Create edit medical history button

--- app/ui/doctor-dashboard/medical-history-btns.tsx
+++ app/ui/doctor-dashboard/medical-history-btns.tsx
@@ -1,7 +1,10 @@
 'use client';
 
 import { useRef } from 'react';
-import MedicalHistoryModal from './medical-history-modal';
+import { PencilIcon } from '@heroicons/react/24/outline';
+import { MedicalHistoryEntry } from '@prisma/client';
+
+import MedicalHistoryModal from '@/app/ui/doctor-dashboard/medical-history-modal';
 
 type CreateMedicalHistoryButtonProps = { doctorId: string; residentId: string };
 
@@ -21,6 +24,39 @@ export function CreateMedicalHistoryButton({
         mode="Create"
         doctorId={doctorId}
         residentId={residentId}
+        ref={dialogRef}
+      />
+    </>
+  );
+}
+
+type EditMedicalHistoryButtonProps = {
+  doctorId: string;
+  residentId: string;
+  medicalHistoryEntry: MedicalHistoryEntry;
+};
+
+export function EditMedicalHistoryButton({
+  doctorId,
+  residentId,
+  medicalHistoryEntry,
+}: EditMedicalHistoryButtonProps) {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  return (
+    <>
+      <button
+        className="btn btn-square btn-sm"
+        onClick={() => dialogRef.current?.showModal()}
+      >
+        <PencilIcon className="h-5" />
+      </button>
+
+      <MedicalHistoryModal
+        mode="Edit"
+        doctorId={doctorId}
+        residentId={residentId}
+        medicalHistoryEntry={medicalHistoryEntry}
         ref={dialogRef}
       />
     </>
