aa93f6bde48ba899c955ace387c454658018926e
William Zheng <williamzheng2001@gmail.com>
2024-04-03 15:03:41 -0400

    Style recent medical history

--- app/ui/doctor-dashboard/recent-medical-history.tsx
+++ app/ui/doctor-dashboard/recent-medical-history.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+
 import { fetchRecentMedicalHistory } from '@/app/lib/data/doctor-data';
 
 type RecentMedicalHistoryProps = { residentId: string };
@@ -9,23 +12,34 @@ export async function RecentMedicalHistory({
   const formatter = new Intl.DateTimeFormat('en-US');
 
   return (
-    <div>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Date</th>
-            <th scope="col">Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          {recentMedicalHistory.map((entry) => (
-            <tr key={entry.id}>
-              <td>{formatter.format(entry.date)}</td>
-              <td>{entry.description}</td>
+    <div className="card card-compact h-full shadow-lg">
+      <div className="card-body">
+        <div className="card-title justify-between">
+          <h2>Recent Medical History</h2>
+          <Link
+            href={`/doctor-dashboard/residents/${residentId}/medical-history`}
+            className="btn btn-square btn-sm"
+          >
+            <ArrowRightIcon className="h-5" />
+          </Link>
+        </div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Date</th>
+              <th scope="col">Description</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {recentMedicalHistory.map((entry) => (
+              <tr key={entry.id}>
+                <td>{formatter.format(entry.date)}</td>
+                <td>{entry.description}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
