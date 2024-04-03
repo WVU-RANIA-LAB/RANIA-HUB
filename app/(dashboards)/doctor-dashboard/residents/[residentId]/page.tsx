a2a06026983ab85a49d5820ed77a017edf11bb4f
William Zheng <williamzheng2001@gmail.com>
2024-04-03 14:08:18 -0400

    Render recent medical history

--- app/(dashboards)/doctor-dashboard/residents/[residentId]/page.tsx
+++ app/(dashboards)/doctor-dashboard/residents/[residentId]/page.tsx
@@ -1,5 +1,17 @@
+import { RecentMedicalHistory } from '@/app/ui/doctor-dashboard/recent-medical-history';
+
 type PageProps = { params: { residentId: string } };
 
 export default function Page({ params }: PageProps) {
-  return <main>{params.residentId}</main>;
+  const { residentId } = params;
+
+  return (
+    <main className="flex grow grid-cols-2 grid-rows-2 flex-col gap-4 bg-white p-4 md:p-8 lg:grid lg:gap-8">
+      <div className="col-start-1 col-end-2 row-start-1 row-end-3 grow bg-gray-100">
+        <RecentMedicalHistory residentId={residentId} />
+      </div>
+      <div className="col-start-2 col-end-3 row-start-1 row-end-2 grow bg-gray-100"></div>
+      <div className="col-start-2 col-end-3 row-start-2 row-end-3 grow bg-gray-100"></div>
+    </main>
+  );
 }
