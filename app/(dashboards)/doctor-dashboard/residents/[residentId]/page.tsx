dee7edea3a2c8107fe29e579e769d71402fdc4c9
William Zheng <williamzheng2001@gmail.com>
2024-04-03 15:03:01 -0400

    Update styles

--- app/(dashboards)/doctor-dashboard/residents/[residentId]/page.tsx
+++ app/(dashboards)/doctor-dashboard/residents/[residentId]/page.tsx
@@ -9,13 +9,13 @@ export default function Page({ params }: PageProps) {
 
   return (
     <main className="flex grow grid-cols-2 grid-rows-2 flex-col gap-4 bg-white p-4 md:p-8 lg:grid lg:gap-8">
-      <div className="col-start-2 col-end-3 row-start-1 row-end-2 grow bg-gray-100">
+      <div className="col-start-2 col-end-3 row-start-1 row-end-2">
         <ResidentInformation residentId={residentId} />
       </div>
-      <div className="col-start-1 col-end-2 row-start-1 row-end-3 grow bg-gray-100">
+      <div className="col-start-1 col-end-2 row-start-1 row-end-3 grow">
         <RecentMedicalHistory residentId={residentId} />
       </div>
-      <div className="col-start-2 col-end-3 row-start-2 row-end-3 grow bg-gray-100">
+      <div className="col-start-2 col-end-3 row-start-2 row-end-3 grow">
         <RecentMedications residentId={residentId} />
       </div>
     </main>
