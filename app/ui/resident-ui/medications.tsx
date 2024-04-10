4cc78311cf1e61c94e274930a6896a1e9b1f9401
Mbehanna2019 <mlb0094@mix.wvu.edu>
2024-04-10 14:05:20 -0400

    got rid of add medications button for residents
    indeed

--- app/ui/resident-ui/medications.tsx
+++ app/ui/resident-ui/medications.tsx
@@ -4,12 +4,7 @@ export default function Medications() {
           <div className="mx-auto max-w-7xl">
             
             <div className={`rounded border-8 border-wvu-primary-blue bg-wvu-primary-blue text-2xl font-bold text-white antialiased flex items-center justify-between`}>
-              <span>Manage Medications</span>
-              <button className="w-40 h-8 text-sm rounded-full bg-white flex justify-center items-center border-2 border-black font-bold hover:bg-gold-500 hover:text-white">
-                <tr className={'text-lg font-bold text-wvu-primary-blue antialiased'}>
-                  Add Medication
-                </tr>
-              </button>
+              <span>Medications</span>
             </div>
     
             <br />
